09a79b8e10f44631ffd6bb7d26b0f1eaccd12ed9
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-05-05 13:05:32 +0800

    Updated server.js
    Removed app.get home page

--- server.js
+++ server.js
@@ -6,16 +6,7 @@ app.set('view engine', 'ejs')
 app.listen(3000);
 
 //Home Page
-app.get('/', (req, res) => {
-    
-    const blogs = [     
-        {title: 'Mia can lift ', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Wala nang roads', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Jonnie Confession', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
 
-    res.render('index', {title: "Home", blogs})
-})
 
 //About page
 app.get('/about', (req, res) => {
